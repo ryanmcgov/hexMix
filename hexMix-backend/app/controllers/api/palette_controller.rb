761c4755201b57af9f6cab8d5abdf90426b795f1
Michelle Hood <meesh121@gmail.com>
2015-07-16 22:09:54 -0400

    tests made, but fail

---
+++ hexMix-backend/app/controllers/api/palette_controller.rb
@@ -0,0 +1,49 @@
+class Api::PalettesController < ApplicationController
+  
+  def index
+    @palettes = Palette.all
+    render json: Palette.all
+  end
+
+  def show
+    @palette = get_palette
+      render :get_palette
+  end
+
+  def create
+    @palettes = Palettes.new(palette_params)
+    if @palette.save
+      render status: 201
+    else
+      render json: @palette.errors, status: 422
+    end
+  end
+
+  def update
+    @palette = get_palette
+    if @palette.update_attributes(palette_params)
+      render :updated
+    else 
+      render json: @palette.errors, status: 422
+    end
+  end
+
+  def destroy
+    @palettes = get_palette
+    if @palette.destroy
+      render head: 204
+    else
+      render json: @palette.errors, status: 422
+    end
+  end
+
+  private
+
+  def palette_params
+    params.require(:palette).permit(:title, :keyword)
+  end
+
+  def get_palette
+    Palette.find(params[:id])
+  end
+end
--- hexMix-backend/test/models/palette_test.rb
+++ hexMix-backend/test/models/palette_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class PaletteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @palette = palettes(:one)
+  end
+
+  test 'fixture is valid' do
+    assert @palette.valid?
+  end
+
+  test 'invalid without title' do
+    @palette.title = nil
+    refute @palette.valid?
+    assert @palette.errors.keys.include?(:title)
+  end
+
+  test 'invalid without keyword' do
+    @palette.keyword = nil
+    refute @palette.valid?
+    assert @palette.errors.keys.include?(:keyword)
+  end
+
+  test 'does not duplicate palette title' do
+    palette = Palette.new(title: 'Test')
+    refute palette.valid?
+    assert @palette.errors.keys.include?(:title)
+  end
+
+  test 'title responds' do
+    assert_respond_to @palette, :title
+  end
+
+  test 'keyword responds' do
+    assert_respond_to @palette, :keyword
+  end
 end
