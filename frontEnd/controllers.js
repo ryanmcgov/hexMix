0e035133ec12de5ac2f25411c9f75132cfb45e8a
datLucius <jlnelsonart@gmail.com>
2015-07-17 12:27:49 -0400

    styling and ng model on palette squares

--- frontEnd/controllers.js
+++ frontEnd/controllers.js
@@ -20,18 +20,4 @@ angular
     },
 
 
-    $scope.createNewPalette = function (title, keyword, hex1, hex2, hex3, hex4, hex5) {
-    	var newPalette = {
-    		el.title : $('#newPaletteTitle').val(),
-    		el.keyword : $('#newPaletteKeyword').val(),
-    		el.hexes.hex1: $('#newPaletteHex1').val(),
-        el.hexes.hex2: $('#newPaletteHex2').val(),
-        el.hexes.hex3: $('#newPaletteHex3').val(),
-        el.hexes.hex4: $('#newPaletteHex4').val(),
-        el.hexes.hex5: $('#newPaletteHex5').val(),
-        }
-        MainController.addPalette(newPalette);
-      };
-
-
 }());
