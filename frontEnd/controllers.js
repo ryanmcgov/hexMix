5b14ff5f6fd637d0347ac593cc873dd240f16274
datLucius <jlnelsonart@gmail.com>
2015-07-17 11:52:01 -0400

    oh snap, things on the page

--- frontEnd/controllers.js
+++ frontEnd/controllers.js
@@ -6,28 +6,29 @@ angular
     PaletteService.getPalettes().success(function (palettes) {
       $scope.palettes = palettes;
     });
+
     $scope.$on('item:added', function() {
       PaletteService.getPalettes().success(function (palettes) {
         $scope.palettes = palettes;
       });
     });
 
-    // $(body).on(‘click’, ‘.submit’, $scope.createNewPallette);
+    $('.submit').on(‘click’, $scope.createNewPallette);
 
     $scope.addPalette = function (newPalette) {
       PaletteService.addPalette(newPalette);
     },
 
 
-    $scope.createNewPallette = function (title, keyword, hex1, hex2, hex3, hex4, hex5) {
+    $scope.createNewPalette = function (title, keyword, hex1, hex2, hex3, hex4, hex5) {
     	var newPalette = {
-    		title : $el.find('input[name="title"]').val(),
-    		keyword: $el.find('input[name="keyword"]').val(),
-    		hex1: $el.find('input[name="hex1"]').val(),
-        hex2: $el.find('input[name="hex2"]').val(),
-        hex3: $el.find('input[name="hex3"]').val(),
-        hex4: $el.find('input[name="hex4"]').val(),
-        hex5: $el.find('input[name="hex5"]').val(),
+    		el.title : $('#newPaletteTitle').val(),
+    		el.keyword : $('#newPaletteKeyword').val(),
+    		el.hexes.hex1: $('#newPaletteHex1').val(),
+        el.hexes.hex2: $('#newPaletteHex2').val(),
+        el.hexes.hex3: $('#newPaletteHex3').val(),
+        el.hexes.hex4: $('#newPaletteHex4').val(),
+        el.hexes.hex5: $('#newPaletteHex5').val(),
         }
         MainController.addPalette(newPalette);
       };
