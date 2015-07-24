fb8183ce011bb0d1f45c20004efa95a93ef09cf2
eastcoastsb <ryanmcgovern@Ryans-MacBook-Air.local>
2015-07-24 10:28:22 -0400

    removed params

--- hexMix-backend/app/controllers/api/hexes_controller.rb
+++ hexMix-backend/app/controllers/api/hexes_controller.rb
@@ -3,4 +3,14 @@ class Api::HexesController < ApplicationController
     @hex = Hex.all
     render json: @hex
   end
+
+  private
+
+  def hexes_params
+    params.require(:hex).permit(:hex1, :hex2, :hex3, :hex4, :hex5)
+  end
+
+  def get_hexes
+    Hex.find(params[:id])
+  end
 end 
